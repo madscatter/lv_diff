ac736223f9a18806a61c69295cfc0c36ab1abc1f
madscatter <madscatter+bitbucket@gmail.com>
2022-11-26 11:08:55 +0900

    deploy to render

--- config/releases.exs
+++ config/releases.exs
@@ -2,7 +2,8 @@
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-use Mix.Config
+#use Mix.Config
+import Config
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
@@ -23,7 +24,7 @@ config :lv_diff, LvDiffWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :lv_diff, LvDiffWeb.Endpoint, server: true
+config :lv_diff, LvDiffWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
